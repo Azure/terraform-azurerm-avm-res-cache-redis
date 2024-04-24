88e294b932136480cf7c3601d1ae16b8410f3b39
Vishal Tewatia <tewatiavishal3@gmail.com>
2024-04-24 14:02:20 +0530

    for_each fix

--- main.accesspolicies.tf
+++ main.accesspolicies.tf
@@ -1,16 +1,16 @@
 #Create the azurerm redis access policy and assignment resources here.
 resource "azurerm_redis_cache_access_policy" "this" {
   for_each       = var.cache_access_policies
-  name           = each.name
-  permissions    = each.permissions
+  name           = each.value.name
+  permissions    = each.value.permissions
   redis_cache_id = azurerm_redis_cache.this
 }
 
 resource "azurerm_redis_cache_access_policy_assignment" "this" {
   for_each           = var.cache_access_policy_assignments
-  name               = each.name
+  name               = each.value.name
   redis_cache_id     = azurerm_redis_cache.this
-  access_policy_name = each.access_policy_name
-  object_id          = each.object_id
-  object_id_alias    = each.object_id_alias
+  access_policy_name = each.value.access_policy_name
+  object_id          = each.value.object_id
+  object_id_alias    = each.value.object_id_alias
 }
