729e07e7049daa495ea235aa7e606685e729eac9
Vishal Tewatia <tewatiavishal3@gmail.com>
2024-04-24 13:14:30 +0530

    add access policies

--- main.accesspolicies.tf
+++ main.accesspolicies.tf
@@ -1,1 +1,16 @@
 #Create the azurerm redis access policy and assignment resources here.
+resource "azurerm_redis_cache_access_policy" "this" {
+  for_each       = var.cache_access_policies
+  name           = each.name
+  permissions    = each.permissions
+  redis_cache_id = azurerm_redis_cache.this
+}
+
+resource "azurerm_redis_cache_access_policy_assignment" "this" {
+  for_each           = var.cache_access_policy_assignments
+  name               = each.name
+  redis_cache_id     = azurerm_redis_cache.this
+  access_policy_name = each.access_policy_name
+  object_id          = each.object_id
+  object_id_alias    = each.object_id_alias
+}
