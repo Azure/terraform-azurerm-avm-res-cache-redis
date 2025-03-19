6b22f4681bb93fd47d160ffa673ad43333cc65c4
Jon Chancellor <jchancellor@microsoft.com>
2025-03-19 14:29:06 -0700

    pre-commit

--- outputs.tf
+++ outputs.tf
@@ -12,8 +12,8 @@ output "private_endpoints" {
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
   description = "This is the full output for the resource."
-  value       = azurerm_redis_cache.this
   sensitive   = true
+  value       = azurerm_redis_cache.this
 }
 
 output "resource_id" {
